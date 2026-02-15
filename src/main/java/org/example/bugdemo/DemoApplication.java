320d68bce0d5e7d113e61f1631e40829a981c86c
Kamil Podlesak <kamil.podlesak@lmc.eu>
2026-02-15 15:08:00 +0100

    basic observability

--- src/main/java/org/example/bugdemo/DemoApplication.java
+++ src/main/java/org/example/bugdemo/DemoApplication.java
@@ -1,11 +1,16 @@
 package org.example.bugdemo;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Hooks;
 
 import java.time.Duration;
 
@@ -15,23 +20,32 @@ import java.time.Duration;
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
+    private final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
     @Autowired
     private WebClient webClient;
+    @Autowired
+    private ObservationRegistry observationRegistry;
 
     @Override
     public void run(String... args) {
-        final ResponseEntity<Void> response = webClient.mutate()
-                .baseUrl("http://www.github.com")
-                .build()
-                .get()
-                .retrieve()
-                .toBodilessEntity()
-                .block(Duration.ofSeconds(5));
-        System.out.printf("response: %s%n", response);
+        final Observation observation = Observation.createNotStarted("test", observationRegistry);
+        observation.observe(() -> {
+            final ResponseEntity<Void> response = webClient.mutate()
+                    .baseUrl("http://www.github.com")
+                    .build()
+                    .get()
+                    .retrieve()
+                    .toBodilessEntity()
+                    .block(Duration.ofSeconds(5));
+            logger.info(String.format("response: %s%n", response));
+        });
+        logger.info(String.format("observation: %s%n", observation));
         System.exit(0);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(DemoApplication.class, args);
     }
 }
